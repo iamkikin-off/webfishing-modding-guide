c1554f94ec4fdfc23b4a1210888a99171ef799d1
IamKikin <kerixc@gmail.com>
2025-05-03 21:27:33 +0200

    Revert "dfgfd"
    This reverts commit 38b3a93c8d60ce57a063690cf8fc93b54174ca72.

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>WEBFISHING Modding Guide</span>,
+  logo: <span>WEBFISHING Modding Docs</span>,
   chat: {
     link: 'https://discord.gg/fWFswUURQX',
   },
